863dc8c54ba3812b7b1a426c75c3a91ed4e4dbd1
= <=>
2023-02-16 16:31:08 +0530

    Image Uploading

--- src/main/java/com/example/EmpCrudDemo/controller/EmpController.java
+++ src/main/java/com/example/EmpCrudDemo/controller/EmpController.java
@@ -2,6 +2,7 @@ package com.example.EmpCrudDemo.controller;
 
 import com.example.EmpCrudDemo.dao.EmpDao;
 import com.example.EmpCrudDemo.entity.Emp;
+import com.example.EmpCrudDemo.helper.FileUploader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -21,6 +23,9 @@ public class EmpController {
     @Autowired
     EmpDao empDao;
 
+    @Autowired
+    FileUploader fileUploader;
+
     @GetMapping("/")
     public String home(Model model)
     {
@@ -29,6 +34,7 @@ public class EmpController {
 
         Page<Emp> pageEmp = empDao.findAll(PageRequest.of(curPage-1, maxSize, Sort.by("name")));
         List<Emp> listEmp = pageEmp.toList();
+
         model.addAttribute("listEmp", listEmp);
         model.addAttribute("curPage", curPage);
         model.addAttribute("totalPages", pageEmp.getTotalPages());
@@ -51,9 +57,15 @@ public class EmpController {
     }
 
     @PostMapping("/emp/save/")
-    public String saveEmp(Model model, Emp emp)
+    public String saveEmp(Model model, Emp emp, MultipartFile file)
     {
-        empDao.save(emp);
+        Emp empSaved = empDao.save(emp);
+
+        String fileNameOld = file.getOriginalFilename();
+        String extension = fileNameOld.substring(fileNameOld.indexOf("."));
+        String fileNameNew = empSaved.getId() + extension;
+
+        fileUploader.uploadFile(file, fileNameNew);
 
         int curPage = 1;
         int maxSize = 5;
